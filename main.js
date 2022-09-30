37cb30ac5dd97d7ae64a471fdc05fd56d2197241
jiyonggg <kimvergognoso21@hufs.ac.kr>
2022-10-01 03:58:16 +0900

    Nav Bar, Article, Footer, changeTheme()

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 // ESLint Test
 
+/**
+ * 라이트-다크 테마 변경
+ */
+function changeTheme() {
+    const bodyElem = document.body;
+
+    switch (bodyElem.className) {
+        case 'light':
+            bodyElem.className = 'dark';
+            break;
+        case 'dark':
+            bodyElem.className = 'light';
+            break;
+        default:
+            bodyElem.className = 'dark';
+            break;
+    }
+}
+
 console.log('Hello World!');
